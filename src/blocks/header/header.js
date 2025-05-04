cfab633145ed6685531bf851a5abfe716a00fa48
Alexandr Tumanov <shurinskiy2@gmail.com>
2025-05-04 22:35:17 +0500

    remove menu

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -3,36 +3,38 @@ import { driveMenu } from "../../js/libs/driveMenu";
 
 (() => {
 	const header = document.querySelector('[data-header-js]');
-	if (! header) return;
-	
 	const navi = header.querySelector('.header__menu');
-	const toggle = header.querySelector('.header__toggle');
-	const close = header.querySelector('.menu__close');
+
+	if ( header && navi) {
+		const toggle = header.querySelector('.header__toggle');
+		const close = header.querySelector('.menu__close');
+		
+		const menu = driveMenu(navi, [toggle, close], {
+			omitToClose: '.modal',
+			class: 'opened',
+			open: function() {
+				disablePageScroll();
+				document.body.classList.add('underlay');
 	
-	const menu = driveMenu(navi, [toggle, close], {
-		omitToClose: '.modal',
-		class: 'opened',
-		open: function() {
-			disablePageScroll();
-			document.body.classList.add('underlay');
-
-			this.querySelectorAll('a.menu__link[href*="#"]').forEach(link => {
-				link.addEventListener('click', (e) => menu.menuClose(e));
-			});
-		},
-		close: function() {
-			enablePageScroll();
-			this.removeAttribute('style');
-			document.body.classList.add('underlay_closing');
-			
-			this.addEventListener('transitionend', e => {
-				document.body.classList.remove('underlay', 'underlay_closing');
-			}, { once: true });
-			
-		}
-	});
-
-	// открытие и закрытие меню, свайпом на мобильных устройствах
-	navi.addEventListener('swiped-left', (e) => menu.menuClose(e));
+				this.querySelectorAll('a.menu__link[href*="#"]').forEach(link => {
+					link.addEventListener('click', (e) => menu.menuClose(e));
+				});
+			},
+			close: function() {
+				enablePageScroll();
+				this.removeAttribute('style');
+				document.body.classList.add('underlay_closing');
+				
+				this.addEventListener('transitionend', e => {
+					document.body.classList.remove('underlay', 'underlay_closing');
+				}, { once: true });
+				
+			}
+		});
+	
+		// открытие и закрытие меню, свайпом на мобильных устройствах
+		navi.addEventListener('swiped-left', (e) => menu.menuClose(e));
+	}
+	
 
 })();
